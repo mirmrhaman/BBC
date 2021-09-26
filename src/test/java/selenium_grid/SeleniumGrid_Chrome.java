107be7601f9a868fd541100cba2c151c31b4c5eb
Mir Masudur Rhaman <mirmrhaman@gmail.com>
2021-09-26 13:39:22 -0400

    update-11

--- src/test/java/selenium_grid/SeleniumGrid_Chrome.java
+++ src/test/java/selenium_grid/SeleniumGrid_Chrome.java
@@ -1,29 +1,30 @@
 package selenium_grid;
 
-import org.junit.Test;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.Test;
 
 
 import java.net.URL;
 
-public class SeleniumGrid_Firefox1 {
+public class SeleniumGrid_Chrome {
     WebDriver driver;
     URL gridUrl;
 
     @Test
     public void executeInAwsDocker() {
-        FirefoxOptions firefoxOptions =new FirefoxOptions();
+        ChromeOptions chromeOptions = new ChromeOptions();
         try {
             gridUrl = new URL("http://3.144.17.205/:4444/wd/hub");
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
 
-        driver = new RemoteWebDriver(gridUrl, firefoxOptions);
+        driver = new RemoteWebDriver(gridUrl, chromeOptions);
         driver.get("https://www.bbc.com/");
         System.out.println(driver.getTitle());
         driver.quit();
     }
+
 }
